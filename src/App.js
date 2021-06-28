9ab8c66fdc61576612f64002976564774a65d95d
Yavuzgrdl <32365733+Yavuzgrdl@users.noreply.github.com>
2021-06-28 21:46:12 +0200

    html todo list and css properties made

--- src/App.js
+++ src/App.js
@@ -3,7 +3,39 @@
 function App() {
   return (
     <div className="App">
-      React ToDo List with Hooks and ContextApi
+      <h1>ToDo List</h1>
+
+      <form autoComplete="off">
+        <input type="text" name="todos" id="todos" required placeholder="What to do?" />
+        <button type="submit">Create</button>
+      </form>
+
+      <ul>
+        <li>
+          <label htmlFor="">
+            <input type="checkbox" id="" />
+            To do
+          </label>
+          <button>Edit</button>
+        </li>
+        <li>
+          <label htmlFor="" >
+            <input type="checkbox" id="" />
+            To do
+          </label>
+          <button>Edit</button>
+        </li>
+      </ul>
+
+      <div className="row">
+        <label htmlFor="all">
+          <input type="checkbox" name="all" id="all" />
+          ALL TODOS
+        </label>
+        <p>You have 0 to do</p>
+        <button id="delete">Delete</button>
+      </div>
+
     </div>
   );
 }
